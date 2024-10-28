6af7b0382b287c0f42165ff8df41c67609679738
AKKOU <creeperkolepa@gmail.com>
2024-10-28 10:23:41 +0800

    f06 & f07

--- test.cpp
+++ test.cpp
@@ -12,6 +12,8 @@ void f02();
 void f03();
 void f04();
 void f05();
+void f06();
+void f07();
 
 
 int main() {
@@ -55,6 +57,12 @@ int main() {
 			case 5:
 				f05();
 				break;
+            case 6:
+                f06();
+                break;
+            case 7:
+                f07();
+                break;
 		}
 		cout<<"\n";
 		system("pause");
@@ -239,4 +247,137 @@ void f05(){
     if(Command == 3 || StateMachine == Stop)
         StateMachine = Exit;
     cout << StateMachine;
+}
+
+void f06(){
+    int i,j;
+    cout << "1.---------------------\n";
+
+    for(int i=0,j=0;i<5;i+=j,j+=2){
+        printf("i=%d j=%d\n",i,j);
+    }
+    printf("變數i=%d\n",i);
+
+    cout << "2.輾轉---------------------\n";
+    int a,b,tmp;
+    printf("GCD=? please input a b:");
+    scanf("%d %d",&a,&b);
+    while(b!=0){
+        tmp = a%b;
+        a=b;
+        b=tmp;
+    }
+    printf("GCD=%d\n",a);
+    cout << "3.99乘法表---------------------\n";
+    for(int i=1;i<10;i++){
+        j=1;
+        if(i==5)
+            continue;
+        do {
+            printf("%2dX%2d=%2d",i,j,i*j);
+            j++;
+            if(j==8)
+                break;
+        }
+        while(j<10);
+        cout << "\n";
+    }
+
+    printf("變數i=%d\n",i);
+    cout << "4.陣列---------------------\n";
+    char name1[] = "blocks";
+    string name2 = "blocks";
+    printf("sizeof(name1)=%d,sizeof(name2)=%d,name2.size()=%d\n",sizeof(name1),sizeof(name2),name2.size());
+
+    int bk1[9] = {1,0,0,1,0,0,1,1,1};
+    int bk2[][3] = {
+        {1,1,1},
+        {0,1,0},
+        {0,1,0}
+    };
+    int bk3[][3] = {1,0,0,1,0,0,1,1,1};
+    int k;
+    string style[] = {" ","■ ","△ "};
+
+    for(i=0;i<9;i++){
+        cout << bk1[i];
+        if((i+1)%3 == 0) cout << endl;
+    }
+    cout << endl;
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cout << style[bk2[i][j]];
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cout << style[bk3[i][j]];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void f07(){
+	int A[] = {2,0,1,8,11,5};
+
+    int n=sizeof(A)/sizeof(int);
+    int i,j,k,tmp,ch=0,c=0;
+    for(i=0; i<n; i++)
+        printf("\t%d",A[i]);
+    cout<<endl;
+    cout << "1.氣泡排序---------------------\n";
+    for(i=n-2; i>=0; i--) {
+        for(j=0; j<=i; j++) {
+            if(A[j] > A[j+1] ) {
+                tmp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = tmp;
+                ch++;
+            }
+            //輸出比較結果
+            cout<<++c<<"\t";    
+            for (k=0; k<n; k++)
+                printf("%d\t",A[k]);
+            cout<<endl;
+        }
+        cout << "\t---------------------\n";
+    }
+    cout << "次數 = "<< c << ",交換次數=" << ch << endl;
+    cout << "請輸入資料筆數:";
+    int N=1;
+    cin>>N;
+    cout << "泡沫排序法比較次數:" << N*(N-1)/2 << "次\n";
+    cout << "2.----------選擇排序法---------------------\n";
+    int min;
+    int B[] = (41,33,17,80,61,5,55);
+    c=0,ch=0;
+
+    n=sizeof(B)/sizeof(int);
+    for(i=0;i<n;i++){
+        printf("\t%d",B[i]);
+    }
+    cout<<endl;
+    for(i=0; i<n; i++) {
+        min=i;
+        for(j=i+1; j<n; j++) {
+            // 找出最小數
+            if(B[min] > B[j] )
+                min=j;
+            c++;
+        }
+        tmp=B[min];
+        B[min]=B[i];
+        B[i]=tmp;
+        for(j=0; j<n; j++)
+            printf("\t%d",B[j]);
+        printf("\n");
+    }
+    cout<<"次數 = "<<c<<endl;
+    cout<<"請輸入資料筆數:";
+    cin>>N;
+    cout<<"選擇排序法比較次數:"<<N*(N-1)/2<<"次"<<endl;
 }
